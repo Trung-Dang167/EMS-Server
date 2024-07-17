f6b680aa1e7156d97f8da28589dde50ab543942a
Trung-Dang167 <dangtrung1607@gmail.com>
2024-07-17 10:04:50 +0700

    UPDATE CODE

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const MODBUS_TCP_PORT = 502;
 const MODBUS_TCP_IP = '192.168.6.231';
 const registerData = 72;
 const registerStatus = 154;
-const numberofRegister = 20;//số thanh ghi luôn chẵn
+const numberofRegister = 24;//số thanh ghi luôn chẵn
 const socketEmitInterval = 3000;//milliseconds
 const intervalHistories = 10000;
 let isModbusConnected = false;
@@ -28,12 +28,14 @@ const tagMap = {
   1: '1HNE10CT201',
   2: '1HNE10CP201',
   3: '1HNE10CQ206',
-  4: '1HNE10CQ207',
-  5: '1HNE10CQ205',
-  6: '1HNE10CQ204',
-  7: '1HNE10CQ203',
-  8: '1HNE10CQ202',
-  9: '1HNE10CQ201',
+  4: '1HNE10CQ205',
+  5: '',
+  6: '',
+  7: '1HNE10CQ204',
+  8: '1HNE10CQ207',
+  9: '1HNE10CQ202',
+  10: '1HNE10CQ201',
+  11: '1HNE10CQ203',
 }
 
 const client = new Modbus();
@@ -47,22 +49,22 @@ function connectModbus(){
 
 async function initializeData(){
   const initialData = [
-    {tag: '1HNE10CQ207', name: 'H20',         unit: 'vol',    status: 'Normal', maxValue:'33', minValue:'0', alarmValue:'28', alarmStatus: 'Normal' },
-    {tag: '1HNE10CQ205', name: 'HCl',         unit: 'mg/Nm3', status: 'Normal', maxValue:'200', minValue:'0', alarmValue:'180', alarmStatus: 'Normal' },
-    {tag: '1HNE10CQ204', name: 'SO2',         unit: 'mg/Nm3', status: 'Normal', maxValue:'500', minValue:'0',  alarmValue:'400', alarmStatus: 'Normal' },
-    {tag: '1HNE10CQ203', name: 'NOx',         unit: 'mg/Nm3', status: 'Normal', maxValue:'800', minValue:'0',  alarmValue:'700', alarmStatus: 'Normal' },
-    {tag: '1HNE10CQ202', name: 'CO',          unit: 'mg/Nm3', status: 'Normal', maxValue:'700', minValue:'0',  alarmValue:'600', alarmStatus: 'Normal' },
-    {tag: '1HNE10CQ201', name: 'O2',          unit: 'vol',    status: 'Normal', maxValue:'21', minValue:'0',  alarmValue:'20', alarmStatus: 'Normal' },
-    {tag: '1HNE10CF201', name: 'Flow',        unit: 'Nm3/s',  status: 'Normal', maxValue:'27', minValue:'0',  alarmValue:'20', alarmStatus: 'Normal' },
-    {tag: '1HNE10CT201', name: 'Temperature', unit: 'oC',     status: 'Normal', maxValue:'200', minValue:'0',  alarmValue:'180', alarmStatus: 'Normal' },
-    {tag: '1HNE10CP201', name: 'Pressure',    unit: 'Pa',     status: 'Normal', maxValue:'1000', minValue:'-500',  alarmValue:'800', alarmStatus: 'Normal' },
-    {tag: '1HNE10CQ206', name: 'Dust',        unit: 'mg/Nm3', status: 'Normal', maxValue:'300', minValue:'0',  alarmValue:'250', alarmStatus: 'Normal' },
+    {tag: '1HNE10CQ207', name: 'H20',         unit: 'vol',    status: 'Normal', maxValue:'33', minValue:'0', alarmValue:'28', alarmStatus: 'Normal', order: 1 },
+    {tag: '1HNE10CQ205', name: 'HCl',         unit: 'mg/Nm3', status: 'Normal', maxValue:'200', minValue:'0', alarmValue:'180', alarmStatus: 'Normal', order: 2 },
+    {tag: '1HNE10CQ204', name: 'SO2',         unit: 'mg/Nm3', status: 'Normal', maxValue:'500', minValue:'0',  alarmValue:'400', alarmStatus: 'Normal', order: 3 },
+    {tag: '1HNE10CQ203', name: 'NOx',         unit: 'mg/Nm3', status: 'Normal', maxValue:'800', minValue:'0',  alarmValue:'700', alarmStatus: 'Normal', order: 4 },
+    {tag: '1HNE10CQ202', name: 'CO',          unit: 'mg/Nm3', status: 'Normal', maxValue:'700', minValue:'0',  alarmValue:'600', alarmStatus: 'Normal', order: 5 },
+    {tag: '1HNE10CQ201', name: 'O2',          unit: 'vol',    status: 'Normal', maxValue:'21', minValue:'0',  alarmValue:'20', alarmStatus: 'Normal', order: 6 },
+    {tag: '1HNE10CF201', name: 'Flow',        unit: 'Nm3/s',  status: 'Normal', maxValue:'27', minValue:'0',  alarmValue:'20', alarmStatus: 'Normal', order: 7 },
+    {tag: '1HNE10CT201', name: 'Temperature', unit: 'oC',     status: 'Normal', maxValue:'200', minValue:'0',  alarmValue:'180', alarmStatus: 'Normal', order: 8 },
+    {tag: '1HNE10CP201', name: 'Pressure',    unit: 'Pa',     status: 'Normal', maxValue:'1000', minValue:'-500',  alarmValue:'800', alarmStatus: 'Normal', order: 9 },
+    {tag: '1HNE10CQ206', name: 'Dust',        unit: 'mg/Nm3', status: 'Normal', maxValue:'300', minValue:'0',  alarmValue:'250', alarmStatus: 'Normal', order: 10 },
   ];
   const connection = await db.getConnection();
   try {
     for (const rowData of initialData){
-      const sql = 'INSERT IGNORE INTO data (tag, name, realtimeValue, unit, time, status, minValue, alarmValue, alarmStatus) VALUES (?,?,?,?,?,?,?,?,?)';
-      const values = [rowData.tag, rowData.name, 0, rowData.unit, 0, rowData.status, rowData.minValue, rowData.alarmValue, rowData.alarmStatus];
+      const sql = 'INSERT IGNORE INTO data (tag, name, realtimeValue, unit, time, status, minValue, alarmValue, alarmStatus, order) VALUES (?,?,?,?,?,?,?,?,?,?)';
+      const values = [rowData.tag, rowData.name, 0, rowData.unit, 0, rowData.status, rowData.minValue, rowData.alarmValue, rowData.alarmStatus, rowData.order];
 
       await connection.query(sql, values);
     }
@@ -89,6 +91,7 @@ function getStatusText(floatStatus) {
 async function updateAlarmStatusText(tag, floatArrayData){
   try {
     const [alarmValueObj] = await data.fetchAlamrValue(tag);
+    // console.log(alarmValueObj);
     const alarmValue = parseFloat(alarmValueObj[0].alarmValue);
     if (isNaN(alarmValue)) {
       console.error(`Giá trị alarm cho tag ${tag} không hợp lệ: ${alarmValueObj[0].alarmValue}`);
@@ -166,6 +169,9 @@ async function readAndWriteData() {
     //Update data to SQL database: 'data' 
     const connection = await db.getConnection();
     for (let i = 0; i < numberofRegister /2; i++) {
+      if (i === 5 || i === 6) {
+        continue; // Bỏ qua các lần lặp với i = 5 và i = 6
+      }
       const floatData = floatArrayData[i];
       const statusText = statusArrayText[i];
       const tag = tagMap[i];
@@ -232,5 +238,5 @@ io.on('connection', (socket) => {
 
 });
 connectModbus();
-initializeData();
+// initializeData();
 setInterval(readAndWriteData, socketEmitInterval);
